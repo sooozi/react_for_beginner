a6a6ea1920284c7179c01ae0beea86a347e0cb88
kimsuji <skme1103@naver.com>
2023-10-15 15:26:54 +0900

    231015_#6.1_end

--- bg_movie_app/src/App.js
+++ bg_movie_app/src/App.js
@@ -1,10 +1,19 @@
 import Button from './Button';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("run all the time!");
+  const onlyOnce = () => {
+    console.log("run only once.")
+  };
+  useEffect(onlyOnce, []);
   return (
     <div className="App">
-      <h1>안녕ㅇㅇ</h1>
-      <Button text={'버튼이다'} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>this</button>
+      {/* <Button text="click me!"></Button> */}
     </div>
   );
 }
