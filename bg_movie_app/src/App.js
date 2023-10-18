31e9df06414878fe79e1e52793d92ccbd0fba0fe
kimsuji <skme1103@naver.com>
2023-10-18 21:38:09 +0900

    231018_#7.5_end

--- bg_movie_app/src/App.js
+++ bg_movie_app/src/App.js
@@ -1,13 +1,14 @@
 //#7.5 React Router
 //리액트 라우터 사용 방법
 //사이트에서 임포트 가져오기
+import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
-import Home from "./routes/Home"
-import Detail from "./routes/Detail"
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   //라우터 컴포넌트 만들어주기
